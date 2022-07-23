2eba386504872c042a307f3ed4ea9c62d16b7120
Michael Legleux <mlegleux@ripple.com>
2022-07-22 19:05:24 -0700

    shut some verbosity up

--- build/build_boost.sh
+++ build/build_boost.sh
@@ -7,5 +7,5 @@ echo "BOOST_VERSION: ${BOOST_VERSION}"
 echo "BOOST_VERSION_: ${BOOST_VERSION_}"
 curl -OJLs "https://boostorg.jfrog.io/artifactory/main/release/${BOOST_VERSION}/source/boost_${BOOST_VERSION_}.tar.gz"
 tar zxvf "boost_${BOOST_VERSION_}.tar.gz"
-cd boost_${BOOST_VERSION_} && ./bootstrap.sh && ./b2 -j$(nproc) >> /boost_build.txt
+cd boost_${BOOST_VERSION_} && ./bootstrap.sh && ./b2 -j$(nproc) >> /boost_build.txt 2>&1
 mkdir -p /boost && mv boost /boost && mv stage /boost
--- build/build_clio.sh
+++ build/build_clio.sh
@@ -4,6 +4,17 @@ export SRC_DIR=$(realpath clio_src)
 echo "[${SRC_DIR}] passed to Clio repo as SRC_DIR"
 
 cmake -S clio_ci -B clio_ci/build -DSRC_DIR=$SRC_DIR
-cmake --build clio_ci/build --parallel $(nproc)
+cmake --build clio_ci/build --parallel $(nproc) >> /clio_build.log 2>&1
+
+ls -lh .
+
+echo "ls -lh clio_ci/build"
+ls -lh clio_ci/build
+
+echo "ls -lh /github/workspace/clio_ci/build"
+ls -lh /github/workspace/clio_ci/build
+
+echo "ls -lh /github/workspace/clio_ci/build/clio-prefix/src/clio-build"
+ls -lh /github/workspace/clio_ci/build/clio-prefix/src/clio-build/
 
 cp /github/workspace/clio_ci/build/clio-prefix/src/clio-build/clio_tests .
