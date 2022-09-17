0958f57926fe318677a97a930d591f4210e9ac73
Michael Legleux <mlegleux@ripple.com>
2022-09-16 18:35:18 -0700

    build rpm

--- build/build_clio.sh
+++ build/build_clio.sh
@@ -3,7 +3,10 @@
 export CLIO_ROOT=$(realpath clio_src)
 echo "[${CLIO_ROOT}] passed to Clio repo as CLIO_ROOT"
 
-cmake -S clio_ci -B clio_ci/build -DCLIO_ROOT=$CLIO_ROOT
+if [ $1 == "rpm" ]:
+    echo "building rpm"
+    gen='rpm'
+cmake -S clio_ci -B -D ${gen} clio_ci/build -DCLIO_ROOT=$CLIO_ROOT
 cmake --build clio_ci/build --parallel $(nproc) | tee -a clio_build.log
 cp /*.log . # get the boost build logs
 cp ./clio_ci/build/clio-prefix/src/clio-build/clio_tests .
